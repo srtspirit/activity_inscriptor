b797d12c27c9969f722c818c9109bce59eeb4176
Vlasov, Andrey <andrey.vlasov@sap.com>
2020-12-27 23:26:38 -0500

    loop for reservation

--- src/main/java/ca/vastier/activityinscriptor/tasks/InscriptionTask.java
+++ src/main/java/ca/vastier/activityinscriptor/tasks/InscriptionTask.java
@@ -3,7 +3,6 @@ package ca.vastier.activityinscriptor.tasks;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,16 +18,17 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
+import static java.util.Optional.ofNullable;
 
 @Component
 public class InscriptionTask implements Task
 {
 	private final RestTemplate restTemplate;
+
+	//TODO inject properties in a better way
 	@Setter
 	@Value("${task.inscriptor.url}")
 	private String mainUrl;
@@ -51,53 +51,62 @@ public class InscriptionTask implements Task
 	public void run(final Map<String, Object> parameters)
 	{
 		final ResponseEntity<String> authenticationResponse = authenticate(parameters);
+		assertSuccessfulLogin(authenticationResponse);
 		final Collection<String> cookiesToPass = extractCookiesWithValuesFromHeaders(authenticationResponse.getHeaders());
 
-		if (isPositiveLogin(authenticationResponse))
+		ResponseEntity<String> bookingResponse;
+		final long delayBetweenTries = ofNullable((Long)parameters.get("delay")).orElse(1000L);
+		do
 		{
-			final ResponseEntity<String> bookingResponse = reserveClass(parameters, authenticationResponse);
-
-			if (isPositiveClassBooking(bookingResponse))
+			try
 			{
-
-
-				final StringBuilder uriSb = new StringBuilder();
-				uriSb.append("https://");
-				uriSb.append((String)parameters.get("domain"));
-				uriSb.append(".");
-				uriSb.append(mainUrl);
-				uriSb.append("/");
-				uriSb.append(bookingConfirmUrl);
-
-				final HttpHeaders confirmBookingHeaders = new HttpHeaders();
-				confirmBookingHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-				confirmBookingHeaders.add("x-requested-with", "XMLHttpRequest");
-
-				cookiesToPass.forEach(cookie -> confirmBookingHeaders.add("cookie", cookie));
-
-				final MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-				map.add("id", (String)parameters.get("eventId"));
-				map.add("dt", (String)parameters.get("dateTime"));
-				map.add("contract_id", findContractNumber());
-				map.add("present_number", (String)parameters.get("present_number")); //not mandatory. null is 1
-
-				final HttpEntity<MultiValueMap<String, String>> bookingRequest = new HttpEntity<>(map, confirmBookingHeaders);
-
-				final ResponseEntity<String> confirmationResponse = restTemplate.postForEntity(uriSb.toString(), bookingRequest, String.class);
-
-				System.out.println(confirmationResponse.getStatusCodeValue());
+				Thread.sleep(delayBetweenTries);
 			}
-			System.out.println(bookingResponse.getStatusCodeValue());
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+			bookingResponse = bookPlaceInEvent(parameters, cookiesToPass);
 		}
-		System.out.println(authenticationResponse.getStatusCodeValue());
+		while (inscriptionIsNotYetOpen(bookingResponse));
+		assertSuccessfulBooking(bookingResponse);
+
+		final ResponseEntity<String> confirmationResponse = confirmBooking(parameters, cookiesToPass);
+		assertSuccessfulConfirmation(confirmationResponse);
 	}
 
-	private ResponseEntity<String> reserveClass(final Map<String, Object> parameters,
-			final ResponseEntity<String> authenticationResponse)
+	private ResponseEntity<String> confirmBooking(final Map<String, Object> parameters, final Collection<String> cookiesToPass)
 	{
 		final StringBuilder uriSb = new StringBuilder();
 		uriSb.append("https://");
-		uriSb.append((String)parameters.get("domain"));
+		uriSb.append((String) parameters.get("domain"));
+		uriSb.append(".");
+		uriSb.append(mainUrl);
+		uriSb.append("/");
+		uriSb.append(bookingConfirmUrl);
+
+		final HttpHeaders confirmBookingHeaders = new HttpHeaders();
+		confirmBookingHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		confirmBookingHeaders.add("x-requested-with", "XMLHttpRequest");
+
+		cookiesToPass.forEach(cookie -> confirmBookingHeaders.add("cookie", cookie));
+
+		final MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+		map.add("id", (String) parameters.get("eventId"));
+		map.add("dt", (String) parameters.get("date"));
+		map.add("contract_id", findContractNumber());
+		map.add("present_number", (String) parameters.get("visitors")); //not mandatory. null is 1
+
+		final HttpEntity<MultiValueMap<String, String>> bookingRequest = new HttpEntity<>(map, confirmBookingHeaders);
+
+		return restTemplate.postForEntity(uriSb.toString(), bookingRequest, String.class);
+	}
+
+	private ResponseEntity<String> bookPlaceInEvent(final Map<String, Object> parameters, final Collection<String> cookiesToPass)
+	{
+		final StringBuilder uriSb = new StringBuilder();
+		uriSb.append("https://");
+		uriSb.append((String) parameters.get("domain"));
 		uriSb.append(".");
 		uriSb.append(mainUrl);
 		uriSb.append("/");
@@ -107,13 +116,11 @@ public class InscriptionTask implements Task
 		bookingHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 		bookingHeaders.add("x-requested-with", "XMLHttpRequest");
 
-		//final Collection<String> cookiesToPass = extractCookiesWithValuesFromHeaders(authenticationResponse.getHeaders(), "AWSALB", "PHPSESSID");
-		final Collection<String> cookiesToPass = extractCookiesWithValuesFromHeaders(authenticationResponse.getHeaders());
 		cookiesToPass.forEach(cookie -> bookingHeaders.add("cookie", cookie));
 
 		final MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-		map.add("id", (String)parameters.get("eventId"));
-		map.add("dt", (String)parameters.get("dateTime"));
+		map.add("id", (String) parameters.get("eventId"));
+		map.add("dt", (String) parameters.get("date"));
 		//map.add("present_number", (String)parameters.get("present_number")); //not mandatory. null is 1
 
 		final HttpEntity<MultiValueMap<String, String>> bookingRequest = new HttpEntity<>(map, bookingHeaders);
@@ -125,7 +132,7 @@ public class InscriptionTask implements Task
 	{
 		final StringBuilder uriSb = new StringBuilder();
 		uriSb.append("https://");
-		uriSb.append((String)parameters.get("domain"));
+		uriSb.append((String) parameters.get("domain"));
 		uriSb.append(".");
 		uriSb.append(mainUrl);
 		uriSb.append("/");
@@ -135,27 +142,27 @@ public class InscriptionTask implements Task
 		loginHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
 		final MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-		map.add("username", (String)parameters.get("username"));
-		map.add("password", (String)parameters.get("password"));
+		map.add("username", (String) parameters.get("username"));
+		map.add("password", (String) parameters.get("password"));
 
 		final HttpEntity<MultiValueMap<String, String>> loginRequest = new HttpEntity<>(map, loginHeaders);
 
 		return restTemplate.postForEntity(uriSb.toString(), loginRequest, String.class);
 	}
 
-	private boolean isPositiveLogin(final ResponseEntity<String> responseEntity)
+	private void assertSuccessfulLogin(final ResponseEntity<String> responseEntity)
 	{
-		if (HttpStatus.FOUND.equals(responseEntity.getStatusCode()) && responseEntity.getHeaders().get("Location").stream().anyMatch(str -> str.contains("/home/dashboard")))
+		if (!HttpStatus.FOUND.equals(responseEntity.getStatusCode()) || !responseEntity.getHeaders()
+				.get("Location")
+				.stream()
+				.anyMatch(str -> str.contains("/home/dashboard")))
 		{
-			return true;
-		}
-		else
-		{
-			return false;
+			//TODO create good exception and pass error description
+			throw new RuntimeException("login failure");
 		}
 	}
 
-	private boolean isPositiveClassBooking(final ResponseEntity<String> responseEntity)
+	private void assertSuccessfulBooking(final ResponseEntity<String> responseEntity)
 	{
 
 		if (HttpStatus.OK.equals(responseEntity.getStatusCode()))
@@ -168,21 +175,83 @@ public class InscriptionTask implements Task
 			}
 			catch (JsonProcessingException e)
 			{
+				//TODO create a good exception
 				e.printStackTrace();
 			}
 
-			if (!objectNode.get("error").booleanValue())
+			if (objectNode.get("error").booleanValue())
+			{
+				//TODO create a good exception
+				throw new RuntimeException("booking failure");
+			}
+		}
+		else
+		{
+			//TODO create a good exception
+			throw new RuntimeException("booking failure");
+		}
+	}
+
+	private void assertSuccessfulConfirmation(final ResponseEntity<String> responseEntity)
+	{
+
+		if (HttpStatus.OK.equals(responseEntity.getStatusCode()))
+		{
+			final ObjectMapper objectMapper = new ObjectMapper();
+			ObjectNode objectNode = null;
+			try
+			{
+				objectNode = objectMapper.readValue(responseEntity.getBody(), ObjectNode.class);
+			}
+			catch (JsonProcessingException e)
+			{
+				//TODO create a good exception
+				e.printStackTrace();
+			}
+
+			if (objectNode.get("error").booleanValue())
+			{
+				//TODO create a good exception
+				throw new RuntimeException("booking failure");
+			}
+		}
+		else
+		{
+			//TODO create a good exception
+			throw new RuntimeException("booking failure");
+		}
+	}
+
+	private boolean inscriptionIsNotYetOpen(final ResponseEntity<String> responseEntity)
+	{
+		if (HttpStatus.OK.equals(responseEntity.getStatusCode()))
+		{
+			final ObjectMapper objectMapper = new ObjectMapper();
+			ObjectNode objectNode;
+			try
+			{
+				objectNode = objectMapper.readValue(responseEntity.getBody(), ObjectNode.class);
+			}
+			catch (JsonProcessingException e)
+			{
+				return false;
+			}
+
+			if (objectNode.get("error").booleanValue() && objectNode.get("message").asText().contains("cours plus de 2 jour(s) (48:00 heures) avant"))
 			{
 				return true;
 			}
 		}
 
-		return false;
+
+			return false;
+
 	}
 
 	private Collection<String> extractCookiesWithValuesFromHeaders(final HttpHeaders headers, final String... searchCookieNames)
 	{
-		return headers.get("set-cookie").stream()
+		return headers.get("set-cookie")
+				.stream()
 				.filter(cookie -> Arrays.stream(searchCookieNames).anyMatch(searchCookie -> cookie.startsWith(searchCookie + '=')))
 				.map(wholeCookie -> wholeCookie.substring(0, wholeCookie.indexOf(';')))
 				.collect(Collectors.toSet());
@@ -190,7 +259,8 @@ public class InscriptionTask implements Task
 
 	private Collection<String> extractCookiesWithValuesFromHeaders(final HttpHeaders headers)
 	{
-		return headers.get("set-cookie").stream()
+		return headers.get("set-cookie")
+				.stream()
 				.map(wholeCookie -> wholeCookie.substring(0, wholeCookie.indexOf(';')))
 				.collect(Collectors.toSet());
 	}
